067d0e12a6dfde5792126ebaebe00797ff2d5117
Drew Gregory <drewgregory@otmail.com>
2022-10-17 14:50:10 -0700

    updated to add additional info

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,18 +4,19 @@
     {
         static void Main(string[] args)
         {
-            var favoriteNumber = 2;
+            var random = new Random();
+            var favoriteNumber = random.Next(1, 1000);
 
-            Console.WriteLine("Try to guess my favorite number");
+            Console.WriteLine("Try to guess my favorite number between 1 - 1000");
             var userInput = int.Parse(Console.ReadLine());
 
             if(userInput < favoriteNumber)
             {
-                Console.WriteLine("Too Low");
+                Console.WriteLine($"Too Low, the number was {favoriteNumber}");
             }
             else if(userInput > favoriteNumber)
             {
-                Console.WriteLine("Too High");
+                Console.WriteLine($"Too High, the number was {favoriteNumber}");
             }
             else
             {
