1edcaa91bdb751a0c641aa761e5266d84c08458e
Drew Gregory <drewgregory@otmail.com>
2022-10-17 14:42:28 -0700

    Exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var favoriteNumber = 2;
+
+            Console.WriteLine("Try to guess my favorite number");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if(userInput < favoriteNumber)
+            {
+                Console.WriteLine("Too Low");
+            }
+            else if(userInput > favoriteNumber)
+            {
+                Console.WriteLine("Too High");
+            }
+            else
+            {
+                Console.WriteLine("Nevermind");
+            }
         }
     }
 }
